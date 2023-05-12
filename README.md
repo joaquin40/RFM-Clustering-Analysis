aa55d123511637a1301dccb2d9a4f84d83e6f6c0
joaquin40 <sanchez.joaquin52@gmail.com>
2023-05-11 18:50:57 -0700

    update Readme

---
+++ README.md
@@ -0,0 +1,85 @@
+# RFM K-means Clustering Analysis
+
+In this project, we focus on creating Recency, Frequency, Monetary (RFM) K-means clustering to identify different groups of customers. The dataset contains online retail transactions from 01/2010 to 09/2011. 
+
+## Data
+The dataset contains 541,909 observations with 8 variables. The dataset also contains about 25% missing values for the variable `CustomerID`. We remove those observations.   
+
+![](./images/missing.png)
+
+Next RFM are calculated for each customer. Recency tells us how recent a customer has made a purchased, frequency is the number of transaction, and monetary is total amount of money a customer has spent. 
+
+
+The table below shows the first 6 customers RFM. For example in the first row, `CustomerID` 12346 has a monetary of 0 dollars with a frequency 2. This means that this particular customer purchased an item and then decided to return it. 
+
+![](./images/summary_df.png)
+
+
+
+Next we look at descriptive statistics of the RFM data. The boxplot below shows that the data is right skewed. 
+
+![](./images/rfm_boxplot.png)
+
+
+Before performing clustering analysis data, we take two approaches: 
+
+1. Log Transformation then scale data 
+2. Remove extreme outliers then scale data
+
+since the data is skewed. We comapre the cluster and choose the better approach to handle the skewed data.
+
+
+## Log Transformation
+
+For the first approach we transformed the data by taking the log transformation then normalizing the data.
+
+Before performing K-means, we calculate the Hopkins statistic, which measures the cluster tendency of the dataset. The  Hopkins statistic is .98, really close to 1. Thus the data contains meaningful clusters.
+
+To decide how many cluster to selected for the K-means method we look at the "Elbow method" and Silhouette plot to get an idea of the number of cluster to select. Based on the two plots we tried k = 3,4,5,6 for the number of cluster.
+
+
+![](./images/wss_log.png)
+![](./images/silhouette_log.png)
+
+The K-means was performed with k = 3,4,5,6. The plots below show the cluster validation using Silhoutte Analysis for k = 3 and k = 4. The Silhoutte value close to 1 indicates the data is cluster well. Values close to zero are poor results.
+
+![](./images/log_val_pca_kmeans_3.png)
+![](./images/log_val_pca_kmeans_4.png)
+![](./images/log_val_pca_kmeans_5.png)
+![](./images/log_val_pca_kmeans_6.png)
+![](./images/log_val_pca_kmeans_7.png)
+
+In the Silhoutte plots above, the red dash line represents the average Silhoutte score. In all Silhoutte plots there at least one label that is below the average Silhoutte score meaning the "k" cluster is not a good choice.
+
+
+Instead of the log transformation, next we choose to remove the extreme outliers from the data then normalize the data. 
+
+Before performing K-means, we calculate the Hopkins statistic, which measures the cluster tendency of the dataset. The  Hopkins statistic is .90, really close to 1. Thus, the data contains meaningful clusters.
+
+Simarly we plot the "Elbow method" and Silhoutte plot to pick a range of candidate for K-means clustering. Based on the two plots we tried k = 3 and 4 for the number of cluster.
+
+![](./images/wss.png)
+![](./images/silhouette.png)
+
+
+In the Silhoutte plots below, the red dash line represents the average Silhoutte score. In all Silhoutte plots, the cluster Silhoutte score are greater than the average Silhoutte score. Between the plots,  k = 3 Silhoutte score has less negative values. Negative values may indicating it may be in the wrong cluster.  
+
+
+![](./images/val_pca_kmeans_3.png)
+![](./images/val_pca_kmeans_4.png)
+
+## RFM Analysis
+
+Below is the cluster plot of the K-means for k = 3.
+
+![](./images/pca_kmeans_3.png)
+
+## Results
+
+The table and bar plot below show the average of RFM by the each cluster. From the RFM analysis, the customers in cluster 3 are the most loyal customers since they frequently buy, spend the most and frequently buy. 
+
+![](./images/rfm_stat_cluster.png)
+
+![](./images/rfm_kmeans3avgRecency.png)
+![](./images/rfm_kmeans3avgFrequency.png)
+![](./images/rfm_kmeans3avgMonetary.png)
--- RFM_Clustering.R
+++ RFM_Clustering.R
@@ -1,42 +1,45 @@
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 knitr::purl("RFM_Clustering.rmd")
 
 
-## ----setup, include=FALSE------------------------------------------------------------------------
+## ----setup, include=FALSE----------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 
-## ------------------------------------------------------------------------------------------------
-pacman::p_load(tidyverse, lubridate, data.table, skimr)
+## ----------------------------------------------------
+pacman::p_load(tidyverse, lubridate, data.table, skimr, cluster, cowplot)
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 df <- fread("./data/Online Retail.csv")
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 df
 skim(df)
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 DataExplorer::plot_missing(df)
+dev.copy(png, "./images/missing.png")
+dev.off()
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 df1 <- drop_na(df)
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 skim(df1)
 
 
-## ------------------------------------------------------------------------------------------------
-max_date <- max(df2$InvoiceDate) + days(1)
-
+## ----------------------------------------------------
 df2 <- df1 %>% 
   mutate(InvoiceDate = dmy_hm(InvoiceDate)) 
 
+max_date <- max(df2$InvoiceDate) + days(1)
+
+# derive recency 
 dates_recency <- df2 %>% 
   group_by(CustomerID) %>% 
   summarise(
@@ -45,7 +48,7 @@ dates_recency <- df2 %>%
   arrange(date_diff) %>% 
   slice(1)
 
-
+# derive freq, monetary and merge with recency
 df3 <- df2 %>% 
   group_by(CustomerID) %>% 
   summarise(frequency = n(),
@@ -55,92 +58,375 @@ df3 <- df2 %>%
   mutate(recency_num = as.numeric(recency)) %>% 
   ungroup()
  
-df3
-
-# normalized rfm
-rfm <- data.frame(scale(df3[,c(2:3,5)]))
-class(rfm)
+df3 %>% head()
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
+skim(df3)
+
+
+## ----------------------------------------------------
+frq <- ggplot(df3, aes(frequency)) + 
+  geom_boxplot() +
+  labs(x = "Frequency") +
+  scale_y_continuous(labels = NULL)+ 
+  theme_classic()
+
+mon <- ggplot(df3, aes(monetary)) + 
+  geom_boxplot() +
+  labs(x = "Monetary") +
+  scale_y_continuous(labels = NULL) +
+  theme_classic()
+
+rec <- ggplot(df3, aes(recency_num)) + 
+  geom_boxplot() +
+  labs(x = "Recency") + 
+  scale_y_continuous(labels = NULL) + 
+  theme_classic()
+
+
+
+ggsave("./images/rfm_boxplot.png",cowplot::plot_grid(frq, mon, rec,ncol = 1))
+
+
+## ----------------------------------------------------
+summary(df3)
+
+
+## ----------------------------------------------------
+df3_positive <- df3 %>% 
+  select(frequency, monetary, recency_num, CustomerID) %>% 
+  filter(monetary > 0)
+
+
+df3_positive_log <- apply(df3_positive[,1:3], 2, log)
+df3_positive_log_scale <- data.frame(scale(df3_positive[,1:3])) %>% 
+  cbind(df3_positive$CustomerID) %>% 
+  rename(CustomerID = "df3_positive$CustomerID")
+
+
+## ----------------------------------------------------
+rfm_log <- df3_positive_log_scale
+
+
+## ----------------------------------------------------
+pacman::p_load(NbClust, clustertend, factoextra,hopkins)
+
+res <- get_clust_tendency(rfm_log[,1:3], n = nrow(rfm_log[,1:3])-1, graph = T, gradient =  list(low = "steelblue", high = "white")
+)
+
+
+## ----------------------------------------------------
+res$hopkins_stat
+
+
+## ----------------------------------------------------
+fviz_nbclust(rfm_log[,1:3], kmeans, method = "wss")+ theme_classic()
+dev.copy(png, "./images/wss_log.png")
+dev.off()
+
+fviz_nbclust(rfm_log[,1:3], kmeans, method = "silhouette")+ theme_classic()
+dev.copy(png, "./images/silhouette_log.png")
+dev.off()
+
+
+## ----------------------------------------------------
+clusternum_log <- NbClust((rfm_log[,1:3]), distance="euclidean", method="kmeans")
+
+
+## ----------------------------------------------------
+set.seed(1)
+km_log <- kmeans(rfm_log[,1:3], 7, nstart = 100, iter.max = 100)
+#km
+
+
+## ----------------------------------------------------
+max_cluster <- max(unique(km_log$cluster))
+
+fviz_cluster(km_log, data = rfm_log[,1:3], palette = c("#FC4E07", "#00AFBB", "#E7B800", "#008A00", "#288BA8", "#E83845", "#846AB0"), ellipse.type = "euclid", 
+star.plot = T, 
+repel = T, 
+ggtheme = theme_minimal() )
+
+dev.copy(png, paste0("./images/log_pca_kmeans_",max_cluster ,".png") )
+dev.off()
+
+
+## ----------------------------------------------------
+sil <-  silhouette(km_log$cluster, dist(rfm_log[,1:3]))
+fviz_silhouette(sil ,palette =c("#FC4E07", "#00AFBB", "#E7B800", "#008A00","#288BA8", "#E83845", "#846AB0"), ggtheme = theme_classic())
+
+dev.copy(png, paste0("./images/log_val_pca_kmeans_", max_cluster, ".png"))
+dev.off()
+
+
+## ----------------------------------------------------
+df3_positive$cluster<- km_log$cluster
+
+
+## ----------------------------------------------------
+df3_positive %>% 
+  group_by(cluster) %>% 
+  count()
+
+
+## ----------------------------------------------------
+rfm_stat <-df3_positive[,c("frequency", "monetary", "recency_num", "cluster")]
+
+
+## ----------------------------------------------------
+rfm_stat_cluster <- rfm_stat %>% 
+  group_by(cluster) %>% 
+  summarise(
+    avgRecency = mean(recency_num),
+    avgFrequency = mean(frequency),
+    avgMonetary = mean(monetary),
+    n = n()
+  ) %>% 
+  arrange(avgRecency, desc(avgMonetary) )
+
+rfm_stat_cluster
+
+
+## ----------------------------------------------------
+stat_name_rfm <- colnames(rfm_stat_cluster[-1])
+
+gg <- function(y){
+  
+  max_cluster <-  max(rfm_stat_cluster[1] )
+    
+p = ggplot(data = rfm_stat_cluster, aes(x= factor(cluster), y = .data[[y]], fill = factor(cluster))) + 
+  geom_bar(stat = "identity") +
+  labs(x = "Cluster", y = paste("Average ", str_sub(y,start = 4) ) ) +
+  theme(legend.position = "none") + 
+  scale_fill_manual(values = c("#FC4E07", "#00AFBB", "#E7B800", "#008A00","#288BA8", "#E83845", "#846AB0")) 
+
+ #ggsave(paste0("./images/", "rfm_log_kmeans", max_cluster, y, ".png"), p)
+ 
+}
+
+# bar plot
+map(stat_name_rfm[1:3], gg)
+
+
+
+## ----------------------------------------------------
+iqr <- apply(df3[,c(2,3,5)], 2, IQR)
+
+lower <-  apply(df3[,c(2,3,5)], 2, quantile, probs = c(0.25)) - 3 * iqr
+upper <- apply(df3[,c(2,3,5)], 2, quantile, probs = c(0.75)) + 3 * iqr
+
+
+## ----------------------------------------------------
+remove_outliers <- df3 %>% 
+  filter(between(frequency, lower[1],upper[1])) %>% 
+  filter(between(monetary, lower[2],upper[2]))  %>%
+  filter(between(recency_num, lower[3],upper[3])) 
+
+
+## ----------------------------------------------------
+rfm <- data.frame(scale(remove_outliers[,c(2:3, 5)] )) %>% 
+  cbind(remove_outliers$CustomerID) %>%
+  rename(CustomerID = "remove_outliers$CustomerID")
+
+
+# unscaled data
+unscaled <- function(x){
+  return(rfm[[x]] * sd(remove_outliers[[x]]) + mean(remove_outliers[[x]]))
+}
+
+col_name <- colnames(rfm[,1:3])
+res <- map(col_name, unscaled)
+
+rfm_unscaled = rfm
+for(i in seq_along(col_name)){
+  rfm_unscaled[col_name[i]] = res[[i]]
+}
+
+
+
+## ---- eval=FALSE, echo=FALSE-------------------------
+## # Scale to remove outliers
+## 
+## rfm <- data.frame(scale(df3[,c(2:3, 5)] )) %>%
+##   cbind(df3$CustomerID) %>%
+##   filter(between(frequency, -3,3)) %>%
+##   filter(between(monetary, -3,3)) %>%
+##   filter(between(recency_num, -3,3)) %>%
+##   rename(CustomerID = "df3$CustomerID")
+## 
+## 
+## 
+## # unscaled data
+## unscaled <- function(x){
+##   return(rfm[[x]] * sd(df3[[x]]) + mean(df3[[x]]))
+## }
+## 
+## col_name <- colnames(rfm[,1:3])
+## res <- map(col_name, unscaled)
+## 
+## rfm_unscaled = rfm
+## for(i in seq_along(col_name)){
+##   rfm_unscaled[col_name[i]] = res[[i]]
+## }
+## 
+## #rfm <- df3[,c(2,3,5)]
+## 
+
+
+## ----------------------------------------------------
 set.seed(1)
 wcss <- c()
-k <-  10
+k <-  15
 
 for(i in 1:k){
-  km = kmeans(rfm, i,  nstart = 100, iter.max = 200)
+  km = kmeans(rfm[,1:3], i+1,  nstart = 100, iter.max = 200)
   wcss[i] = km$tot.withinss
 }
 
-ggplot(data.frame(x = 1:k, wcss = wcss), aes(x = x, y = wcss)) + 
+ggplot(data.frame(x = 2:(k+1), wcss = wcss), aes(x = x, y = wcss)) + 
   geom_point() + 
   geom_line() + 
   scale_x_continuous(breaks = (1:k)) + 
   labs(x = "Number of Cluster (k)", y = "Within-cluster sum of squares", title = "") 
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
 set.seed(1)
-km <- kmeans(rfm, 7, nstart = 100, iter.max = 200)
+k_max <- 15
+
+sil_fun <- function(k) {
+  km <- kmeans(rfm[,1:3], centers = k, nstart = 200, iter.max = 500)
+  ss <- silhouette(km$cluster, dist(rfm[,1:3]))
+  return(mean(ss[,3]))
+}
+
+sil_width_mean_list <- map(2:k_max, sil_fun)
 
 
-## ------------------------------------------------------------------------------------------------
+
+
+## ----------------------------------------------------
+sil_width_mean <- unlist(sil_width_mean_list)
+
+
+## ----------------------------------------------------
+# Plot the results
+plot(2:k_max, sil_width_mean, type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters (k)", ylab = "Silhouette Width")
+
+
+## ----------------------------------------------------
+pacman::p_load(NbClust, clustertend, factoextra,hopkins)
+
+res <- get_clust_tendency(rfm[,1:3], n = nrow(rfm[,1:3])-1, graph = T, gradient =  list(low = "steelblue", high = "white")
+)
+
+
+## ----------------------------------------------------
+res$hopkins_stat
+
+
+## ----------------------------------------------------
+res$plot
+dev.copy(png, "./images/hopkins_plot.png")
+dev.copy()
+
+
+## ----------------------------------------------------
+fviz_nbclust(rfm[,1:3], kmeans, method = "wss")+ theme_classic()
+dev.copy(png, "./images/wss.png")
+dev.off()
+
+fviz_nbclust(rfm[,1:3], kmeans, method = "silhouette")+ theme_classic()
+dev.copy(png, "./images/silhouette.png")
+dev.off()
+
+
+## ----------------------------------------------------
+clusternum <- NbClust((rfm[,1:3]), distance="euclidean", method="kmeans")
+
+
+## ----------------------------------------------------
+set.seed(1)
+km <- kmeans(rfm[,1:3], 3, nstart = 100, iter.max = 100)
+#km
+
+
+## ----------------------------------------------------
+max_cluster <- max(unique(km$cluster))
+
+fviz_cluster(km, data = rfm[,1:3], palette = c("#FC4E07", "#00AFBB", "#E7B800", "#008A00", "#288BA8", "#E83845", "#846AB0"), ellipse.type = "euclid", 
+star.plot = T, 
+repel = T, 
+ggtheme = theme_minimal() )
+
+dev.copy(png, paste0("./images/pca_kmeans_",max_cluster ,".png") )
+dev.off()
+
+
+## ----------------------------------------------------
+sil <-  silhouette(km$cluster, dist(rfm[,1:3]))
+fviz_silhouette(sil ,palette =c("#FC4E07", "#00AFBB", "#E7B800", "#008A00","#288BA8", "#E83845", "#846AB0"), ggtheme = theme_classic())
+
+dev.copy(png, paste0("./images/val_pca_kmeans_", max_cluster, ".png"))
+dev.off()
+
+
+## ----------------------------------------------------
 km
 
-## ------------------------------------------------------------------------------------------------
-df3$cluster <- km$cluster 
 
-df3 %>% select(CustomerID, cluster)
-
-
-## ------------------------------------------------------------------------------------------------
-#rfm$clsuter <- km$cluster
-rfm_stat <- df3[,c(2:3,5:6)]
+## ----------------------------------------------------
+#df3$cluster  <- km$cluster 
+rfm_unscaled$cluster  <- km$cluster 
 
 
 
-## ------------------------------------------------------------------------------------------------
-rfm_stat
+## ----------------------------------------------------
+rfm_unscaled %>% 
+  group_by(cluster) %>% 
+  count()
 
-rfm_stat %>% 
+
+## ----------------------------------------------------
+rfm_stat <- rfm_unscaled[,c("frequency", "monetary", "recency_num", "cluster")]
+
+
+## ----------------------------------------------------
+rfm_stat_cluster <- rfm_stat %>% 
   group_by(cluster) %>% 
   summarise(
     avgRecency = mean(recency_num),
     avgFrequency = mean(frequency),
-    avgMonetary = mean(monetary)
+    avgMonetary = mean(monetary),
+    n = n()
   ) %>% 
   arrange(avgRecency, desc(avgMonetary) )
 
+rfm_stat_cluster
 
 
-## ------------------------------------------------------------------------------------------------
+## ----------------------------------------------------
+stat_name_rfm <- colnames(rfm_stat_cluster[-1])
+
+gg <- function(y){
+  
+  max_cluster <-  max(rfm_stat_cluster[1] )
+    
+p = ggplot(data = rfm_stat_cluster, aes(x= factor(cluster), y = .data[[y]], fill = factor(cluster))) + 
+  geom_bar(stat = "identity") +
+  labs(x = "Cluster", y = paste("Average ", str_sub(y,start = 4) ) ) +
+  theme(legend.position = "none") + 
+  scale_fill_manual(values = c("#FC4E07", "#00AFBB", "#E7B800", "#008A00","#288BA8", "#E83845", "#846AB0")) 
+
+ ggsave(paste0("./images/", "rfm_kmeans", max_cluster, y, ".png"), p)
+
+}
+
+y ="avgRecency"
+# bar plot
+map(stat_name_rfm[1:3], gg)
 
 
-a <- df2 %>% 
-  group_by(CustomerID) %>% 
-  summarise(
-    days = difftime(max(InvoiceDate),InvoiceDate , units = "days"))
-
-hours <- (as.numeric(a$days) %% 1) *24
-minutes <- (hours %% 1) * 60
-
-paste0(floor(a$days), " days ", floor(hours), "  ", floor(minutes), " minutes")
-
-
-
-date1 <- as.POSIXct("2022-05-20 12:00:00")
-date2 <- as.POSIXct("2022-05-24 10:30:00")
-
-# Calculate difference in days
-diff_days <- as.numeric(difftime(date2, date1, units = "days"))
-
-# Calculate remaining time in seconds
-diff_seconds <- as.numeric(difftime(date2, date1, units = "secs"))
-
-# Convert remaining seconds into hours and minutes format
-diff_hm <- format(as.POSIXct(diff_seconds, origin = "1970-01-01", tz = "GMT"), format = "%H:%M")
-
-# Combine the results
-diff_time <- paste0(diff_days, " days ", diff_hm)
-diff_time
 
